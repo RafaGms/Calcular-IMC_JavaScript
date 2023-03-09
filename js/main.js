36e569b2cb60213a46bc57cd1339be1942d12fd5
RafaGms <faelgomes2003@gmail.com>
2023-03-09 20:56:59 -0300

    complete calculator

--- js/main.js
+++ js/main.js
@@ -1,53 +1,54 @@
 const calcular = document.getElementById("calcular");
 
 calcular.addEventListener("click", function () {
-  const altura = document.getElementById("altura").value;
-  const peso = document.getElementById("peso").value;
-  
-  if (!Number.isInteger(altura)) {
-    var newNumber = altura / 100;
-    console.log(newNumber);
-    var calculo = peso / (newNumber * newNumber);
-    respostaIMC(calculo);
-  } else {
-    var n1 = parseFloat(altura.replace(",", "."));
-    var calculo = peso / (n1 * n1);
-    console.log(n1);
-    respostaIMC(calculo);
-  }
- 
-});
+  var altura = document.getElementById("altura").value;
+  var peso = document.getElementById("peso").value;
+  var result = document.getElementById("resposta");
 
-function respostaIMC(resultado) {
-  const result = document.getElementById("resposta");
-  if (!isNaN(resultado)) {
-    if (resultado < 18.5) {
-      result.style.visibility = "visible";
-      result.style.color = "#71b9db";
-      return (result.innerHTML = "Magreza");
-    } else if (resultado < 24.9) {
-      result.style.visibility = "visible";
-      result.style.color = "#73db71";
-      return (result.innerHTML = "Normal");
-    } else if (resultado < 29.9) {
-      result.style.visibility = "visible";
-      result.style.color = "#dbcb71";
-      return (result.innerHTML = "Sobrepeso");
-    } else if (resultado < 34.9) {
-      result.style.visibility = "visible";
-      result.style.color = "#db9d71";
-      return (result.innerHTML = "Obesidade grau I");
-    } else if (resultado < 39.9) {
-      result.style.visibility = "visible";
-      result.style.color = "#db7c71";
-      return (result.innerHTML = "Obesidade grau II");
-    } else {
-      result.style.visibility = "visible";
-      result.style.color = "#e94545";
-      return (result.innerHTML = "Obesidade grau III");
+  if (altura != "" && peso != "") {
+    altura = parseFloat(altura.replace(",", "."));
+    peso = parseFloat(peso.replace(",", "."));
+    if (altura > 3) {
+      altura = altura / 100;
     }
+
+    var imc = peso / Math.pow(altura, 2)
+
+    return respostaIMC(imc);
   } else {
     result.style.visibility = "visible";
-    return (result.innerHTML = "Sem Valor");
+    result.style.color = "#ff0000";
+    return (result.innerHTML = "Sem Valor, preencrer os campos!")
+  }
+
+});
+
+function respostaIMC(imc) {
+  const result = document.getElementById("resposta");
+
+  if (imc < 18.5) {
+    result.style.visibility = "visible";
+    result.style.color = "#71b9db";
+    return (result.innerHTML = "Magreza");
+  } else if (imc < 24.9) {
+    result.style.visibility = "visible";
+    result.style.color = "#73db71";
+    return (result.innerHTML = "Normal");
+  } else if (imc < 29.9) {
+    result.style.visibility = "visible";
+    result.style.color = "#dbcb71";
+    return (result.innerHTML = "Sobrepeso");
+  } else if (imc < 34.9) {
+    result.style.visibility = "visible";
+    result.style.color = "#db9d71";
+    return (result.innerHTML = "Obesidade grau I");
+  } else if (imc < 39.9) {
+    result.style.visibility = "visible";
+    result.style.color = "#db7c71";
+    return (result.innerHTML = "Obesidade grau II");
+  } else {
+    result.style.visibility = "visible";
+    result.style.color = "#e94545";
+    return (result.innerHTML = "Obesidade grau III");
   }
 }
